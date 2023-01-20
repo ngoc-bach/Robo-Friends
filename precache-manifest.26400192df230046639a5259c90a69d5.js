09147e538f6e5aad297c8faec03d86ca0e350a1e
Rachel Bach <thanhngocbach0904@gmail.com>
2023-01-20 16:45:51 +1100

    Updates

---
+++ precache-manifest.26400192df230046639a5259c90a69d5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "c1ed0373697d5b7bfed1",
+    "url": "/Robo-Friends/static/css/main.1eade0b1.chunk.css"
+  },
+  {
+    "revision": "c1ed0373697d5b7bfed1",
+    "url": "/Robo-Friends/static/js/main.c1ed0373.chunk.js"
+  },
+  {
+    "revision": "ee2fa2fc286181834050",
+    "url": "/Robo-Friends/static/js/1.ee2fa2fc.chunk.js"
+  },
+  {
+    "revision": "f78249525f81cb7bb3af",
+    "url": "/Robo-Friends/static/css/2.524faea1.chunk.css"
+  },
+  {
+    "revision": "f78249525f81cb7bb3af",
+    "url": "/Robo-Friends/static/js/2.f7824952.chunk.js"
+  },
+  {
+    "revision": "8b8302fb5d9aa9d9a2ca",
+    "url": "/Robo-Friends/static/js/runtime~main.8b8302fb.js"
+  },
+  {
+    "revision": "cf41de0a4adf3e80eaf3547fa80892c0",
+    "url": "/Robo-Friends/static/media/Lazer Game Zone.cf41de0a.woff"
+  },
+  {
+    "revision": "aa13142dd1c09d917d5cf63982000216",
+    "url": "/Robo-Friends/index.html"
+  }
+];
